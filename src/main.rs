78c4368d008d03adffc2cf54e544845a19745bba
its-popplewell <39974659+its-popplewell@users.noreply.github.com>
2023-08-25 19:37:12 -0400

    Match With Online

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::process::Command;
+
+use std::io;
+use std::io::prelude::*;
+
+struct FileName {
+    filename: String,
+    fileextension: String,
+    path: String,
+}
+
+impl FileName {
+    fn new(filename: String) -> FileName {
+        FileName {
+            filename,
+            fileextension,
+            path,
+        }
+    }
+}
+
+
+fn read_file_bytes(str::)
+
+
 fn main() {
-    println!("Hello, world!");
+
+
+
+    // Command::new("cd")
+    //     .arg("cd /")
+    //     .spawn()
+    //     .expect("Failed to execute command");
+
+    // Command::new("ls")
+    //     .spawn()
+    //     .expect("Failed to LS");
+
 }
